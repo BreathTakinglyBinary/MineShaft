60fd2033ef218eda9f48161a26befc6bcf618c01
95CivicSi <aaron_salyer@yahoo.com>
2020-02-05 05:33:27 -0500

    Add MineShaftConfiguration.  Internal file used for storing config.yml variables.

---
+++ src/p2e/mineshaft/MineShaftConfiguration.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+
+namespace p2e\mineshaft;
+
+
+class MineShaftConfiguration{
+
+    public const REFILL_TYPE_PERCENT = 0;
+
+    public const REFILL_TYPE_TIME = 1;
+
+    private $autoRefill = false;
+
+    private $entireWorld = false;
+
+    private $protectionEnabled = false;
+
+    private $refillType = self::REFILL_TYPE_PERCENT;
+
+    private $refillPercentage = 5;
+
+    private $refillInterval = 600;
+
+    /**
+     * @param bool $autoRefill
+     */
+    public function enableAutoRefill(bool $autoRefill) : void{
+        $this->autoRefill = $autoRefill;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAutoRefillEnabled() : bool{
+        return $this->autoRefill;
+    }
+
+    /**
+     * @param bool $entireWorld
+     */
+    public function setEntireWorldProtectionEnabled(bool $entireWorld) : void{
+        $this->entireWorld = $entireWorld;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEntireWorldProtectionEnabled() : bool{
+        return $this->entireWorld;
+    }
+
+    /**
+     * @param bool $protectionEnabled
+     */
+    public function enableProtection(bool $protectionEnabled) : void{
+        $this->protectionEnabled = $protectionEnabled;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isProtectionEnabled() : bool{
+        return $this->protectionEnabled;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRefillInterval() : int{
+        return $this->refillInterval;
+    }
+
+    /**
+     * @param int $refillInterval
+     */
+    public function setRefillInterval(int $refillInterval) : void{
+        if(!($refillInterval <= 90 and 1 <= $refillInterval)){
+
+        }
+        $this->refillInterval = $refillInterval;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRefillPercentage() : int{
+        return $this->refillPercentage;
+    }
+
+    /**
+     * @param int $refillPercentage
+     */
+    public function setRefillPercentage(int $refillPercentage) : void{
+        $this->refillPercentage = $refillPercentage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRefillType() : int{
+        return $this->refillType;
+    }
+
+    /**
+     * @param int $refillType
+     */
+    public function setRefillType(int $refillType) : void{
+        $this->refillType = $refillType;
+    }
+}
